bdddc3041b97f005a4df88dd267492c3550b7e9f
MHaseebKhalid <38959887+MHaseebKhalid@users.noreply.github.com>
2021-08-02 05:39:22 -0700

    make it better

--- routes/students.js
+++ routes/students.js
@@ -1,20 +1,14 @@
 import express from 'express';
 
-import {addStudent,getStudents,getStudent,deleteStudent,updateStudent} from '../controllers/students.js';
+import {addStudent, getStudents, getStudent, deleteStudent, updateStudent} from '../controllers/students.js';
 
 //initialize routes
 const router = express.Router();
 
 
-
-
-
 // get request from main
 //all routes here are starting with /students i.e; /students == /
 router.get('/', getStudents);
-
-
-
 
 //Post request
 router.post('/', addStudent);
@@ -23,18 +17,11 @@ router.post('/', addStudent);
 //  '/:id'  is because we'r expecting something after /
 router.get('/:id',getStudent);
 
-
-
 //Delete request
 router.delete('/:id',deleteStudent);
-
-
-
 
 //Update request
 //we'r using PATCH method because we want to change data partially, we'r not using PUT because PUT override previous data
 router.patch('/:id',updateStudent);
-
-
 
 export default router; 
